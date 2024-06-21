5027c32a7f2127f2d7ac59905a15ce61bc3c4ba3
fmpberger88 <fabian.berger@protonmail.ch>
2024-06-21 09:47:25 +0200

    new fields for isAuthor

--- models/users.js
+++ models/users.js
@@ -15,8 +15,17 @@ const userSchema = new mongoose.Schema({
             message: props => `${props.value} is not a valid email!`
         }
     },
-    password: { type: String, required: true }
-});
+    password: {
+        type: String,
+        required: true },
+    isAuthor: {
+        type: Boolean,
+        default: false
+    },
+},
+    {
+        timestamps: true
+    });
 
 userSchema.pre('save', async function(next) {
     if (!this.isModified('password')) return next();
