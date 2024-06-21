decbe793c4c3f7dffe90855769d4fcef7097497b
fmpberger88 <fabian.berger@protonmail.ch>
2024-06-21 16:32:24 +0200

    define errorHandler

---
+++ middlewares/errorHandler.js
@@ -0,0 +1,21 @@
+// errorHandler.js
+
+function errorHandler(err, req, res) {
+    console.error(err.stack); // Log error to console; consider using a more advanced logging solution for production
+
+    const errorResponse = {
+        message: err.message,
+        ...(process.env.NODE_ENV === 'development' && { stack: err.stack })
+    };
+
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+
+    res.status(statusCode).json({
+        success: false,
+        error: errorResponse,
+        // Provide stack trace only in development
+        stack: process.env.NODE_ENV === 'development' ? err.stack : undefined
+    });
+}
+
+module.exports = errorHandler;
