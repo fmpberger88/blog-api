24194a1f752f45b2987606b6d50468598013c9ea
fmpberger88 <fabian.berger@protonmail.ch>
2024-07-15 08:49:16 +0200

    implement category model

---
+++ models/category.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+const categorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please enter a name'],
+        unique: true,
+        trim: true,
+        maxLength: [50, 'Title cannot be more than 50 characters']
+    },
+    description: {
+        type: String,
+        required: [true, 'Please enter a description'],
+        trim: true,
+        maxLength: [200, 'Description cannot be more than 200 characters']
+    }
+}, {
+    timestamps: true
+});
+
+const Category = mongoose.model('Category', categorySchema);
+module.exports = Category;
