e9e53e5bc83504bc2b25e7d7455b0adf681959c2
fmpberger88 <fabian.berger@protonmail.ch>
2024-08-02 13:22:21 +0200

    send json

--- middlewares/isAdmin.js
+++ middlewares/isAdmin.js
@@ -2,7 +2,7 @@ const isAdmin = (req, res, next) => {
     if (req.isAuthenticated() && req.user.isAdmin) {
         next();
     } else {
-        return res.status(403).send('Access denied. Admins only.');
+        return res.status(403).json({ message: 'Access denied. Admins only.'});
     }
 }
 
