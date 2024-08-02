e2a7373b6587bc07f23d0665ce682406f1507112
fmpberger88 <fabian.berger@protonmail.ch>
2024-08-02 14:47:29 +0200

    add to callback

--- middlewares/passport.js
+++ middlewares/passport.js
@@ -5,19 +5,20 @@ const User = require('../models/users');
 const options = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     secretOrKey: process.env.JWT_SECRET,
-}
+    passReqToCallback: false, // Hier auf false setzen, wenn der Request nicht benötigt wird
+};
 
 passport.use(new Strategy(options, async (jwt_payload, done) => {
     try {
         const user = await User.findById(jwt_payload.id);
         if (user) {
             return done(null, user);
+        } else {
+            return done(null, false); // Kein Fehler, aber Benutzer nicht gefunden
         }
-        return done(null, false);
     } catch (error) {
-        return done(error);
+        return done(error, false); // Fehler beim Finden des Benutzers
     }
 }));
-
 
 module.exports = passport;
