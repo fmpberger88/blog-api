ef785408f5b2273263548f10f3a8e13c0adaf363
fmpberger88 <fabian.berger@protonmail.ch>
2024-08-10 21:35:08 +0200

    fix import

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { body, validationResult } = require('express-validator');
-const Category = require('../models/Category');
+const Category = require('../models/category');
 const isAdmin = require('../middlewares/isAdmin');
 const customPassportAuth = require('../middlewares/customPassportAuth');
 
