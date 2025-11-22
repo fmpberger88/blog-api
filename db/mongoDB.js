6acb3790e5c122acdbe9d3a8202bb65473d0a25b
fmpberger88 <fabian.berger@protonmail.ch>
2025-11-22 22:48:39 +0100

    delete deprivate options

--- db/mongoDB.js
+++ db/mongoDB.js
@@ -11,8 +11,6 @@ if (!MONGO_URI) {
 mongoose.set('strictQuery', false);
 
 mongoose.connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
 })
     .then(() => {
         console.log('✅ Connected successfully to MongoDB!');
